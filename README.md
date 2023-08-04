e1f8c9656f4d515015b04a2067afb053fa661c28
[Keval Patel] <[khp90611@gmail.com]>
2023-08-03 23:22:48 -0400

    bad commit message by accident on the previous commit. Figured out localStorage issue with tutors help. Finished Readme and credited tutor. Added comments in js file. Deleted uneccesary files from folders

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # Work-Day-Scheduler
-This repository is for a simple calendar application that allows users to save events during a typical work day. 
+
+
+# Description
+This repository is for a simple calendar application that allows users to save events during a typical work day in hourly blocks. The application was created in order to provide users the ability to create a calendar for their work day and save those items so that the next time they load the page the events will appear in the time slots in which they were saved. As a software developer, from creating this application, I was able to test my fundamentals of JavaScript and learn to use DOM APIs to manipulate HTML elements. I was also able to showcase my understanding of localStorage and how it can be used to save information and load it on the page when needed.
+
+# Usage
+In order to use this application, first, you will need to go to the application landing page which can be found here, <placeholder>. On the landing page you can see the current date and the day of the week. If you scroll down you can see the hours of a typical work day (9am-5pm). 
+
+As a user, you will be able to click on the time slot text area and type in anything that you want. Typically, a user will fill out an event or a to-do item in this text area. After typing in an event or to-do item, you will need to hit the save button with a save icon on the right hand side of the text area in order for the event to be saved. 
+
+Below you can see a screenshot of the landing page with the current date and day of the week at the top. Circled in red on the right, you can see the save icon, which will save the Test Event to local storage for the appropriate time slot. Upon refresh (circled in red on the left), the Test event will be loaded onto the page. 
+
+On page refresh the event text should populate in the appropriate time slot. 
+
+![Alt text](assets/save-button.PNG)
+
+Additionally, the work day is color coded to reflect the past, present, and future events. The gray background colored time slots indicate past events for hours that have already passed by. The red background color indicates the present hour and the green background color represents future events.
+
+As an example of the above color coordination, you can see the screen shot below with the current hour set to 12pm. 
+
+![Alt text](assets/background-colors.PNG)
+
+## Credits
+
+Credit to my tutor Joem Casusi for helping me with the local storage issue I was having.
+
+
+License
+MIT License
+
+Copyright (c) [2023] [Keval Patel]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- assets/script.js
+++ assets/script.js
@@ -1,145 +1,79 @@
-//---------------Global variables--------------------------------------------------------//
-let currentDay = document.querySelector("#currentDay");
-let currentDate = document.querySelector("#currentDate")
-let allTextArea = document.querySelectorAll('textarea');
+//----------------------jQuery Wrapper so HTML loads before executing function-----------------//
 
-//----------------------------------------------------------------------------------------//
+$(function () {
 
-//-----------Step 1 Set the date at the top of the page-------------------------------//
-// TODO: Add code to display the current date in the header of the page.
+  
+  //---------------------------Setting Date at the top of the page--------------------------//
+  let currentDay = $("#currentDay")
+  let currentDate = document.querySelector("#currentDate")
 
-currentDay.textContent = dayjs().format("MMMM D, YYYY");
-currentDate.textContent = dayjs().format("dddd");
-
-
-//----------------------------------------------------------------------------------------//
-//-----------Step 2 Find Current Time and Compare it to the Hour shown-------------------------------//
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
-
-//.........................Current hour in military time.............//
-let currentHour = dayjs().hour();
-///---------------------------------------------------------------///
-//----------Set variable = all time blocks and so it is an array---//
-let hourBlocks = document.querySelectorAll('.time-block')
+  currentDay.text(dayjs().format("MMMM D, YYYY"));
+  currentDate.textContent = dayjs().format("dddd");
 
 
-//----------------------Comparison to Id's----------------------//
-//Need to run a for loop since I grabbed all time blocks and now it is an array//
-for (let i = 0; i < hourBlocks.length; i++){
 
-  if (currentHour==hourBlocks[i].dataset.time){
-    hourBlocks[i].className = "row time-block present"
-  } 
-  else if (currentHour<hourBlocks[i].dataset.time){
+
+  
+  //---------------------------------Color change of Time Blocks----------------------------//
+
+
+ 
+  let currentHour = dayjs().hour()
+  let hourBlocks = document.querySelectorAll('.time-block')
+
+
+  for (let i = 0; i < hourBlocks.length; i++) {
+
+    if (currentHour == hourBlocks[i].dataset.time) {
+      hourBlocks[i].className = "row time-block present"
+    }
+    else if (currentHour < hourBlocks[i].dataset.time) {
       hourBlocks[i].className = "row time-block future"
     }
-  else {
-    hourBlocks[i].className = "row time-block past"
+    else {
+      hourBlocks[i].className = "row time-block past"
+    }
+
   }
-    
-}
 
-//-----------------------------------------------------------------------
+ 
 
-//-------------Event Listener for Save Button............................//
+  //------------------------------Event Listener for Save Button---------------------------//
 
-
-
-
-
-
-
-
-
-
-//----------------------------------------------------------------------------------------//
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
   let saveButton = document.querySelector('.time-block-container');
 
-  //------------------setting all local storage values to an empty string//
-  
-  
-  saveButton.addEventListener('click', function(event){
-    
-    
-    if (event.target.matches("button")){
+
+  saveButton.addEventListener('click', function (event) {
+
+
+    if (event.target.matches("button")) {
       let eventText = event.target.previousElementSibling.value
       let timeBlockNumber = event.target.dataset.time
-      
+
       localStorage.setItem(timeBlockNumber, eventText)
     }
-  
-    else if (event.target.matches('i')){
+
+    else if (event.target.matches('i')) {
       let eventText = event.target.parentElement.previousElementSibling.value
       let timeBlockNumber = event.target.dataset.time
-      
+
       localStorage.setItem(timeBlockNumber, eventText)
     }
 
   })
- 
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // let calendarEvents = localStorage.getItem() || null;
-  for (i=0; i<10; i++){
-      console.log(allTextArea)
-     if(localStorage.key(i)!==null){
-      console.log(localStorage.key(i))
-      allTextArea[i] = localStorage.getItem(localStorage.key(i))
-      }
-    
-    else{
-      allTextArea[i].textContent = ''
-      }
+
+//-----Getting the text values from local storage that were saved using the save button----//
+
+  let allTextArea = document.querySelectorAll('textarea');
+
+  for (i = 9; i < 18; i++) {
+    console.log(i)
+
+    let textValue = localStorage.getItem(i)
+
+    if (textValue != null) {
+      allTextArea[i - 9].value = textValue;
     }
-  // if(localStorage.getItem(i)===null){
-    
-  // }
-  // else{
-  //   let targetTextArea = document.textarea.querySelector("data-time")
-  //   localStorage.getItem
-  // }
-  
-  // // Get the saved item to show up on the page now//
-  // // /---------------attempt 1-------------//
-  // function pageLoad(){
-  //   let allTextArea = document.querySelectorAll('textarea')
-  // for (let i = 0; i < allTextArea.length; i++) {
-  //       if(allTextArea[i].dataset.time===localStorage.key(allTextArea[i])){
-  //         allTextArea[i].value = localStorage.getItem(i)
-  //       }
-  
-  
-  // }}
-  //---------------attempt 2------------//
-  //On page load//
-
-  //   let allTextArea = document.querySelectorAll('textarea.data-time')
-  //   for (let i = 0; i < allTextArea.length; i++) {
-    //     if (allTextArea[i].dataset.time==localStorage.key(i+9)){
-      //       allTextArea[i].value = localStorage.getItem(i+9) 
-      //       console.log(localStorage.getItem(i))
-      //     }
-      //     else if(!allTextArea[i].dataset.time==localStorage.key(i+9)){
-        //       allTextArea[i].value = ""
-//     }  
-//   }
 
 
-})
+}})
